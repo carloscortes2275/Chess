65f677ab0b6c0adaf2690058e242500f1630abde
Carlos Cortés <carlos_cortes-2275@hotmail.com>
2024-04-30 10:07:04 -0600

    Hola mundo

---
+++ reconocimiento_de_piezas.py
@@ -0,0 +1,5 @@
+#Creación de sistema de reconocimiento de ajedrez con cámara web o de celular
+#Capaz de reconocer las piezas asi como a que jugador pertenecen
+#También deberá ser capaz de reconocer movimientos y la puntuación que lleva el juego.
+
+print('Hello World!')
